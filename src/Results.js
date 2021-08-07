57d499db4bdd57e78fd55b18cb69538eb16d513f
Xasaur <75995544+Xasaur@users.noreply.github.com>
2021-08-07 19:16:58 -0400

    Add files via upload

---
+++ src/Results.js
@@ -0,0 +1,16 @@
+import "./App.css"
+
+export const Results = () => {
+    return (
+        <div class='results'>
+        <div class='results-card'>
+          <p id="event">Boys 13-14 50 Free SCY</p>
+          <p id="time">25:78</p>
+          <div>
+            <p id="points">498 Points</p>
+            <p id="cut">BB Cut</p>
+          </div>
+        </div>
+      </div>
+    )
+}
---
+++ src/Title.js
@@ -0,0 +1,9 @@
+import "./App.css"
+
+export const Title = () => {
+    return (
+        <div class='title'>
+            <h1>SwimSync</h1> 
+        </div>
+    )
+}
