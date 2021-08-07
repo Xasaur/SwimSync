f50410381f7974c0d243924805e7e640a0298fca
Xasaur <75995544+Xasaur@users.noreply.github.com>
2021-08-07 19:16:22 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Results } from './Results';
+import { Search } from './Search';
+import { Title } from './Title';
+
+function App() {
+  return (
+    <div>
+      <Title/ >
+      <Search/ > 
+      <Results/ >
+    </div>
+  );
+}
+export default App;
